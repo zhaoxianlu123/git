575b5b75d62f1d72d2d47685228f10a75737092b
86136 <376521721@qq.c>
2020-01-13 15:24:04 +0800

    添加测试配置

--- src/test/java/com/example/demo1/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo1/Demo1ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo1;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Demo1ApplicationTests {
 
+    @Test
     public void contextLoads() {
     }
 
